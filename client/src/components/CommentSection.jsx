216948675c35045289ff5705682e03f7624799d3
Vlad <f.a.s.t.i.k.19801@gmail.com>
2024-06-07 23:21:26 +0200

    add edit func to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -80,6 +80,13 @@ export default function CommentSection({postId}) {
         }
     }
 
+    const handleEdit = async (comment, editContent) => {
+        setComments(
+            comments.map((c) => 
+            c._id === comment._id ? { ...c, content: editContent} : c)
+        )
+    }
+
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
         {currentUser ? (
@@ -135,6 +142,7 @@ export default function CommentSection({postId}) {
                             key={comment._id}
                             comment={comment}
                             onLike={handleLike}
+                            onEdit={handleEdit}
                         />
                     ))
                 }
